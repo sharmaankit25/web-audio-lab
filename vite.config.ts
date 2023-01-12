948b4128553c70105b5ad447d6609ba298b76953
Ankit Sharma <ankit.sharma25@gmail.com>
2023-01-12 23:07:29 +0530

    hg

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/scribble/",
+  base: "/web-audio-lab/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
